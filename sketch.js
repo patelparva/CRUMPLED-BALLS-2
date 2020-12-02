e37bb6cb8d724d35ada1fdd1a3c66947ac310040
patelparva <74237707+patelparva@users.noreply.github.com>
2020-12-02 15:24:57 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,11 +4,6 @@ const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
-function preload()
-{
-	dimage=loadImage("dustbingreen.png");
-}
-
 function setup() {
 	createCanvas(1200, 700);
 
@@ -17,10 +12,7 @@ function setup() {
 
 	paper=new Paper(100,200,40);
 	ground = new Ground(width/2,680,width,20);
-	b1=new Dustbin(900,610,10,120);
-	b2=new Dustbin(950,660,120,10);
-	b3=new Dustbin(1000,610,10,120);
-
+	b1=new Dustbin(840,660);
 
 	Engine.run(engine);
   
@@ -36,16 +28,10 @@ function draw() {
   paper.display();
   ground.display();
   b1.display();
-  b2.display();
-  b3.display();
-
-  image(dimage,890,550,120,120);
-
-  keyPressed();
 }
 
 function keyPressed() {
-	if (keyDown("up")) {
-		Matter.Body.applyForce(paper.body,paper.body.position,{x:52,y:-52});
+	if (keyCode===UP_ARROW) {
+		Matter.Body.applyForce(paper.body,paper.body.position,{x:100,y:-100});
 	}	
 }
